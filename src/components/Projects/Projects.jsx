01b1e1429afc7de005194d1988ab36ea5103ca43
Atta Jirofty <jirofty76@gmail.com>
2021-11-08 00:03:39 +0330

    Projects updated

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -15,11 +15,11 @@ const Projects = () => {
         </div>
         <div className="row">
           <div className="col-12 col-sm-6 col-lg-3 projects-items">
-            <NavLink to="/projects/archi-cv-sample">
+            <NavLink to="/projects/react/archi-cv-sample">
               <div className="img-middle-div text-center">
                 <img
                   className="img-projects-present"
-                  src="./images/Projects/Arch CV.jpg"
+                  src="./images/Projects/react.png"
                   alt="Architecture CV Sample"
                 />
                 <div className="middle">
@@ -30,11 +30,11 @@ const Projects = () => {
             </NavLink>
           </div>
           <div className="col-12 col-sm-6 col-lg-3 projects-items">
-            <NavLink to="/projects/my-cv">
+            <NavLink to="/projects/react/my-cv">
               <div className="img-middle-div text-center">
                 <img
                   className="img-projects-present"
-                  src="./images/Projects/my CV.gif"
+                  src="./images/Projects/react.png"
                   alt="My CV"
                 />
                 <div className="middle">
@@ -45,11 +45,11 @@ const Projects = () => {
             </NavLink>
           </div>
           <div className="col-12 col-sm-6 col-lg-3 projects-items">
-            <NavLink to="/projects/profile-intern-task">
+            <NavLink to="/projects/html-css-js/profile-intern-task">
               <div className="img-middle-div text-center">
                 <img
                   className="img-projects-present"
-                  src="./images/Projects/Profile.png"
+                  src="./images/Projects/html-css-js.jpg"
                   alt="My CV"
                 />
                 <div className="middle">
@@ -60,11 +60,11 @@ const Projects = () => {
             </NavLink>
           </div>
           <div className="col-12 col-sm-6 col-lg-3 projects-items">
-            <NavLink to="/projects/mother-nature">
+            <NavLink to="/projects/bootstrap/mother-nature">
               <div className="img-middle-div text-center">
                 <img
                   className="img-projects-present"
-                  src="./images/Projects/mother nature.png"
+                  src="./images/Projects/boostrap.png"
                   alt="Mother Nature"
                 />
                 <div className="middle">
@@ -75,11 +75,11 @@ const Projects = () => {
             </NavLink>
           </div>
           <div className="col-12 col-sm-6 col-lg-3 projects-items">
-            <NavLink to="/projects/pet-nurse">
+            <NavLink to="/projects/wordpress/pet-nurse">
               <div className="img-middle-div text-center">
                 <img
                   className="img-projects-present"
-                  src="./images/Projects/pet nurse.png"
+                  src="./images/Projects/wordpress.png"
                   alt="Pet Nurse"
                 />
                 <div className="middle">
@@ -102,75 +102,90 @@ const Projects = () => {
         </div>
         <div className="row">
           <div className="col-12 col-sm-6 col-lg-3 projects-items">
-            <NavLink to="/projects/archi-cv-sample">
+            <NavLink to="/projects/unity/realm-rush">
               <div className="img-middle-div text-center">
                 <img
                   className="img-projects-present"
-                  src="./images/Projects/Arch CV.jpg"
-                  alt="Architecture CV Sample"
+                  src="./images/Projects/unity.png"
+                  alt="Unity Project"
                 />
                 <div className="middle">
-                  <h4>Architecture CV Sample</h4>
+                  <h4>Realm Rush</h4>
                   <h5>Click For Details</h5>
                 </div>
               </div>
             </NavLink>
           </div>
           <div className="col-12 col-sm-6 col-lg-3 projects-items">
-            <NavLink to="/projects/my-cv">
+            <NavLink to="/projects/unity/argon-assault">
               <div className="img-middle-div text-center">
                 <img
                   className="img-projects-present"
-                  src="./images/Projects/my CV.gif"
-                  alt="My CV"
+                  src="./images/Projects/unity.png"
+                  alt="Unity Project"
                 />
                 <div className="middle">
-                  <h4>My CV</h4>
+                  <h4>Argon Assault</h4>
                   <h5>Click For Details</h5>
                 </div>
               </div>
             </NavLink>
           </div>
           <div className="col-12 col-sm-6 col-lg-3 projects-items">
-            <NavLink to="/projects/profile-intern-task">
+            <NavLink to="/projects/unity/project-boost">
               <div className="img-middle-div text-center">
                 <img
                   className="img-projects-present"
-                  src="./images/Projects/Profile.png"
-                  alt="My CV"
+                  src="./images/Projects/unity.png"
+                  alt="Unity Project"
                 />
                 <div className="middle">
-                  <h4>Profile Intern Project</h4>
+                  <h4>Project Boost</h4>
                   <h5>Click For Details</h5>
                 </div>
               </div>
             </NavLink>
           </div>
           <div className="col-12 col-sm-6 col-lg-3 projects-items">
-            <NavLink to="/projects/mother-nature">
+            <NavLink to="/projects/cplus/tetris-game">
               <div className="img-middle-div text-center">
                 <img
                   className="img-projects-present"
-                  src="./images/Projects/mother nature.png"
-                  alt="Mother Nature"
+                  src="./images/Projects/c plus.jpg"
+                  alt="Tetris Game"
                 />
                 <div className="middle">
-                  <h4>Mother nature</h4>
+                  <h4>Tetris</h4>
                   <h5>Click For Details</h5>
                 </div>
               </div>
             </NavLink>
           </div>
           <div className="col-12 col-sm-6 col-lg-3 projects-items">
-            <NavLink to="/projects/pet-nurse">
+            <NavLink to="/projects/cplus/accounting-software">
               <div className="img-middle-div text-center">
                 <img
                   className="img-projects-present"
-                  src="./images/Projects/pet nurse.png"
-                  alt="Pet Nurse"
+                  src="./images/Projects/c plus.jpg"
+                  alt="Accounting Software"
                 />
                 <div className="middle">
-                  <h4>Pet Nurse</h4>
+                  <h4>Accounting Software</h4>
+                  <h5>Click For Details</h5>
+                </div>
+              </div>
+            </NavLink>
+          </div>
+          <div className="col-12 col-sm-6 col-lg-3 projects-items">
+            <NavLink to="/projects/cplus/arduino/parking-sensor">
+              <div className="img-middle-div text-center">
+                <img
+                  className="img-projects-present"
+                  src="./images/Projects/Arduino.png"
+                  alt="Parking Sensor"
+                />
+                <div className="middle">
+                  <h4>Parking Sensor</h4>
                   <h5>Click For Details</h5>
                 </div>
               </div>
@@ -189,47 +204,66 @@ const Projects = () => {
           <h2>Other Shit</h2>
         </div>
         <div className="row">
-          <div className="col-12 col-sm-6 col-md-4 projects-items">
-            <NavLink to="/projects/fun/drawings">
-              <img
-                className="img-projects-present"
-                src="./images/Projects/Just For Fun/Drawings/Drawing.jpg"
-                alt="Drawings"
-              />
-              <div className="middle">
-                <h4>BG gen</h4>
-                <h5>Click For Details</h5>
+          <div className="col-12 col-sm-6 col-lg-3 projects-items">
+            <NavLink to="/projects/react/todo-app">
+              <div className="img-middle-div text-center">
+                <img
+                  className="img-projects-present"
+                  src="./images/Projects/react.png"
+                  alt="Todo App"
+                />
+                <div className="middle">
+                  <h4>Todo App</h4>
+                  <h5>Click For Details</h5>
+                </div>
               </div>
             </NavLink>
           </div>
-          <div className="col-12 col-sm-6 col-md-4 projects-items">
-            <NavLink to="/projects/fun/sketches">
-              <img
-                className="img-projects-present"
-                src="./images/Projects/Just For Fun/Sketches/Sketch.jpg"
-                alt="Sketches"
-              />
-              <div className="middle">
-                <h4>Robo friends</h4>
-                <h5>Click For Details</h5>
+          <div className="col-12 col-sm-6 col-lg-3 projects-items">
+            <NavLink to="/projects/react/person-manager">
+              <div className="img-middle-div text-center">
+                <img
+                  className="img-projects-present"
+                  src="./images/Projects/react.png"
+                  alt="Person Manager"
+                />
+                <div className="middle">
+                  <h4>Person Manager</h4>
+                  <h5>Click For Details</h5>
+                </div>
               </div>
             </NavLink>
           </div>
-
-          <div className="col-12 col-sm-6 col-md-4 projects-items">
-            <NavLink to="/projects/fun/software-practice">
-              <img
-                className="img-projects-present"
-                src="./images/Projects/Just For Fun/Software Practice/SOFTWARE 1.jpg"
-                alt="Sketches"
-              />
-              <div className="middle">
-                <h4>info form </h4>
-                <h5>Click For Details</h5>
+          <div className="col-12 col-sm-6 col-lg-3 projects-items">
+            <NavLink to="/projects/react/robo-friends">
+              <div className="img-middle-div text-center">
+                <img
+                  className="img-projects-present"
+                  src="./images/Projects/react.png"
+                  alt="My CV"
+                />
+                <div className="middle">
+                  <h4>Robo Friends</h4>
+                  <h5>Click For Details</h5>
+                </div>
               </div>
             </NavLink>
           </div>
-          <hr className="mt-3" />
+          <div className="col-12 col-sm-6 col-lg-3 projects-items">
+            <NavLink to="/projects/html-css-js/bg-generator">
+              <div className="img-middle-div text-center">
+                <img
+                  className="img-projects-present"
+                  src="./images/Projects/html-css-js.jpg"
+                  alt="BG Generator"
+                />
+                <div className="middle">
+                  <h4>Background Generator</h4>
+                  <h5>Click For Details</h5>
+                </div>
+              </div>
+            </NavLink>
+          </div>
         </div>
       </div>
     </Fragment>
