259ff6513e01270d5fbff1d80b47706f2c8fe5a0
Atta Jirofty <jirofty76@gmail.com>
2021-11-09 09:52:32 +0330

    Profile intern task added

--- src/components/Projects/Front-end Projects/ProfileInternTask.jsx
+++ src/components/Projects/Front-end Projects/ProfileInternTask.jsx
@@ -9,59 +9,45 @@ const ProfileInternTask = () => {
             <h1>Profile Intern Task</h1>
             <img
               className="img-project-indiv"
-              src="../../images/Projects/Front-end Projects/my-cv.png"
+              src="../../images/Projects/Front-end Projects/profile-intern-task.png"
               alt="Architecture CV Sample"
             />
           </div>
           <div className="col-12 pb-5">
             <h2>About This Project</h2>
             <p className="p-project-indiv">
-              This is my second React base website.
-              <br />
-              <br />I did my best to implement all the standard methods for
-              making a React App, but I'm still working on it to make a pure,
-              standard, state of the art React project.
+              This is my Internship task in Profile Compay.
               <br />
               <br />
-              I'm still learning a lot about how React works and how I can make
-              it even better.
+              My task was to implement this website by only using Flexbox, to
+              make it responsive for all resolutions. I did my best to do that.
               <br />
               <br />
-              When I first started this project, it was a pure html,css,js
-              project
+              No Bootstrap or other frameworks were used in this project.
+              <br />
+              <br />
+              It was a great practice to understand Flexbox and its capabilities
+              thourouly.
+              <br />
+              <br />
               <a
                 className="link-project-indiv"
-                href="https://github.com/attajey/My-CV"
+                href="https://profile.ir/"
                 target="__blank"
               >
-                (find source code here)
+                (Click to visit Profile official website)
               </a>
-              but after I learned more about React, I decided to start from
-              skratch and make a duplicate project by using React and better css
-              styling. My css code lines where half the before after I
-              reconsidered all of it.
-              <br />
-              <br />
-              For future work, I want to completely separate the components from
-              each other. Right now for example, if you want to make a change in
-              image galleries that is used in different pages, you should change
-              it in each component one by one, which is absolutely now following
-              the DRY philosephy. Other future changes are similar to this one.
-              <br />
-              <br />I will constantly update my github repo. You can check it
-              out and see the changes in my commits. I will be honored if you
-              could find a bug or issue and tell me about it.
             </p>
             <a
               className="link-project-indiv"
-              href="http://www.google.com"
+              href="https://attajey.github.io/Profile/"
               target="__blank"
             >
               <button className="btn btn-dark">find live demo here</button>
             </a>
             <a
               className="link-project-indiv"
-              href="https://github.com/attajey/mycv"
+              href="https://github.com/attajey/Profile"
               target="__blank"
             >
               <button className="btn btn-dark">find source code here</button>
