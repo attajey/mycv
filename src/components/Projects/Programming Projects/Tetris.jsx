8ec17fcaf79409c99eac47af3fcba7e09b45ac3f
Atta Jirofty <jirofty76@gmail.com>
2021-11-13 18:36:48 +0330

    All Projects Added

---
+++ src/components/Projects/Programming Projects/Tetris.jsx
@@ -0,0 +1,47 @@
+import React, { Fragment } from "react";
+import { NavLink } from "react-router-dom";
+
+const MyCv = () => {
+  return (
+    <Fragment>
+      <div className="container">
+        <div className="row">
+          <div className="col-1">
+            <NavLink
+              to="/projects"
+              className="fa fa-backward fa-2x back-button"
+            ></NavLink>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 text-center py-3">
+            <h1>Tetris</h1>
+            {/* <video className="project-video" controls>
+              <source
+                src="../../images/Projects/Programming Projects/Argon Assault.mp4"
+                type="video/mp4"
+              />
+              Your browser does not support the video tag.
+            </video> */}
+          </div>
+          <div className="col-12 pb-5">
+            <h2>About This Project</h2>
+            <p className="p-project-indiv">
+              This Project Is Under Construction !
+            </p>
+
+            <a
+              className="link-project-indiv"
+              href="https://github.com/attajey/Tetris-Game"
+              target="__blank"
+            >
+              <button className="btn btn-dark">find source code here</button>
+            </a>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default MyCv;
--- src/containers/MainCv.jsx
+++ src/containers/MainCv.jsx
@@ -13,11 +13,42 @@ import MotherNature from "../components/Projects/Front-end Projects/MotherNature
 import ProjectBoost from "../components/Projects/Programming Projects/ProjectBoost";
 import ArgonAssault from "../components/Projects/Programming Projects/ArgonAssault";
 import RealmRush from "../components/Projects/Programming Projects/RealmRush";
+import Tetris from "../components/Projects/Programming Projects/Tetris";
+import AccountingSoftware from "../components/Projects/Programming Projects/AccountingSoftware";
+import ParkingSensor from "../components/Projects/Programming Projects/ParkingSensor";
+import TodoApp from "../components/Projects/Other Shit/TodoApp";
+import PersonManager from "../components/Projects/Other Shit/PersonManager";
+import RoboFriends from "../components/Projects/Other Shit/RoboFriends";
+import BgGenerator from "../components/Projects/Other Shit/BgGenerator";
 
 const MainCv = (props) => {
   return (
     <MainLayout>
       <Switch>
+        <Route
+          path="/projects/html-css-js/bg-generator"
+          component={BgGenerator}
+        />
+
+        <Route path="/projects/react/robo-friends" component={RoboFriends} />
+        <Route
+          path="/projects/react/person-manager"
+          component={PersonManager}
+        />
+
+        <Route path="/projects/react/todo-app" component={TodoApp} />
+
+        <Route
+          path="/projects/cplus/arduino/parking-sensor"
+          component={ParkingSensor}
+        />
+        <Route
+          path="/projects/cplus/accounting-software"
+          component={AccountingSoftware}
+        />
+
+        <Route path="/projects/cplus/tetris-game" component={Tetris} />
+
         <Route path="/projects/unity/realm-rush" component={RealmRush} />
 
         <Route path="/projects/unity/argon-assault" component={ArgonAssault} />
