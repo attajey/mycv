200e4be392da63dec2a48bfe4cb07218562fee74
Atta Jirofty <jirofty76@gmail.com>
2021-11-16 14:56:10 +0330

    build added

---
+++ build/precache-manifest.584f5a765bfde4c5c0d5a031cb0a94cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d426ea37ef9a37b51ac67765b042e3a",
+    "url": "/mycv/index.html"
+  },
+  {
+    "revision": "377eb434e673c4ee3fa9",
+    "url": "/mycv/static/js/2.7268b29f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/mycv/static/js/2.7268b29f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c6dbabe8a3843bdd178",
+    "url": "/mycv/static/js/main.dd0ba481.chunk.js"
+  },
+  {
+    "revision": "3d830aa802aa3c9e97fd",
+    "url": "/mycv/static/js/runtime-main.e52f963b.js"
+  }
+]);
